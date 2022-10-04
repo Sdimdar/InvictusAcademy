85766cf4275535f6c328440965485a85895b5021
Madiar <madiar.bakbergenov10@hmail.com>
2022-10-05 02:08:44 +0600

    #8 Создал МиниСервисы пагинации и вьюмодели страницы и страницы юзеров ----------------------------------------------------------- Созданы IPaginationService.cs, IUsersPaginationService.cs, UsersPaginationService.cs, UsersDataVm.cs, PageVm.cs

---
+++ src/Services/Identity/Identity.Application/Features/Services/UsersPaginationService.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Application.Features.Services;
+
+public class UsersPaginationService
+{
+    public async Task<(IQueryable<User>, int)> GetABatchOfData(IQueryable<User> users, int page, int pageSize)
+    {
+        int count = await users.CountAsync();
+        return (users.OrderByDescending(v => v.RegistrationDate).Skip((page - 1) * pageSize).Take(pageSize), count);
+    }
+}
