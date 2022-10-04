88e1872e17ed572a51f60ea374bbec677812674d
Madiar <madiar.bakbergenov10@hmail.com>
2022-10-05 03:40:40 +0600

    #8 Создал пагинацию в Endpoint и Querries

---
+++ src/Services/Identity/Identity.API/Endpoints/User/GetUsersData.cs
@@ -0,0 +1,35 @@
+﻿using Ardalis.ApiEndpoints;
+using Ardalis.Result;
+using Ardalis.Result.AspNetCore;
+using Identity.Application.Features.Users.Queries.GetUserData;
+using Identity.Application.Features.Users.Queries.GetUsersData;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Identity.API.Endpoints.User;
+
+public class GetUsersData : EndpointBaseAsync
+    .WithRequest<int>
+    .WithResult<Result<UsersDataVm>>
+{
+    private readonly IMediator _mediator;
+
+    public GetUsersData(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("/user/getUsersData")]
+    [TranslateResultToActionResult]
+    [SwaggerOperation(
+        Summary = "Получение данных пользователей",
+        Description = "Для пагинации требуется вести в строку номер страницы",
+        Tags = new[] { "User" })
+    ]
+    public override async Task<Result<UsersDataVm>> HandleAsync(int page, CancellationToken cancellationToken = default)
+    {
+        GetUsersDataQuerry command = new(page);
+        return await _mediator.Send(command, cancellationToken);
+    }
+}
---
+++ src/Services/Identity/Identity.Application/Features/Users/Queries/GetUsersData/GetUsersDataQuerry.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Result;
+using Identity.Application.Features.Users.Queries.GetUserData;
+using MediatR;
+
+namespace Identity.Application.Features.Users.Queries.GetUsersData;
+
+public class GetUsersDataQuerry : IRequest<Result<UsersDataVm>>
+{
+    public int Page { get; set; }
+
+    public GetUsersDataQuerry(int page)
+    {
+        Page = page;
+    }
+}
