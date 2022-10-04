b9572572a2bfab65f5360abec4539980e8906fc9
miha <miha890r@gmail.com>
2022-10-04 12:51:07 +0600

    Добавление новой миграции с добавлением строки AvatarLink (refactor)

---
+++ src/Services/Identity/Identity.Infrastructure/Migrations/20221004033319_AvatarLinkAdd.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Infrastructure.Migrations
+{
+    public partial class AvatarLinkAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarLink",
+                table: "AspNetUsers",
+                type: "VARCHAR(60)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarLink",
+                table: "AspNetUsers");
+        }
+    }
+}
