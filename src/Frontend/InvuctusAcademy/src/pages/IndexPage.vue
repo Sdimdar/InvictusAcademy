d901b236994903344d6c9f6a112db34887f22d3b
Svoyevolin Dmitriy <sdimdar@gmail.com>
2022-10-07 19:44:07 +0600

    Починил кнопка заявки, появляется только для незалогинного пользователя(fix)

--- src/Frontend/InvuctusAcademy/src/pages/IndexPage.vue
+++ src/Frontend/InvuctusAcademy/src/pages/IndexPage.vue
@@ -1,46 +1,23 @@
 <template>
   <q-page-container v-if="!logined">
-    <request-button :logined="logined" @autorize="autorize" />
+    <request-button />
   </q-page-container>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import RequestButton from 'components/RequestButton.vue'
-import { fetchLoginedUserData } from 'boot/axios'
 
 export default defineComponent({
   name: 'IndexPage',
   components: {
     RequestButton
   },
-  data() {
-    return {
-      logined: false,
-      loginedUserEmail: ""
-    }
-  },
-  methods: {
-    autorize: function (email) {
-      this.logined = true;
-      this.loginedUserEmail = email;
+  props: {
+    logined: {
+      type: Boolean,
+      required: true,
     },
-    unautorize: function () {
-      this.logined = false;
-      this.loginedUserEmail = ""
-    },
-    async checkLogin() {
-      try {
-        const response = await fetchLoginedUserData();
-        this.autorize(response.data.email);
-      } catch (e) {
-        console.log(e.message);
-        this.unautorize()
-      }
-    }
-  },
-  mounted() {
-    this.checkLogin()
   }
 })
 </script>
