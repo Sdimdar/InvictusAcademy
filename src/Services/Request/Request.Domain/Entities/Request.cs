a7985679ab1bddff14c25eb071d89544647a79cc
Svoyevolin Dmitriy <sdimdar@gmail.com>
2022-10-09 01:20:30 +0600

    #11 указал в сущности при создании добавление метки времени о создании(fix)

--- src/Services/Request/Request.Domain/Entities/Request.cs
+++ src/Services/Request/Request.Domain/Entities/Request.cs
@@ -8,6 +8,6 @@ public class Request:BaseRepositoryEntity
     public string UserName { get; set; }
     public string PhoneNumber { get; set; }
     public string? ManagerComment { get; set; }
-    public DateTime CreatedDate { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
     public bool WasCalled { get; set; } = false;
 }
