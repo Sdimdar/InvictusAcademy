16fb3ef0b47d6ae0d490c20891bd576c7b5fd8a0
Svoyevolin Dmitriy <sdimdar@gmail.com>
2022-10-09 18:21:49 +0600

    #11 Обработка изменения статуса WasCalled(feature) - Добавил обработку изменения статуса поля WasCalled для Request - Требуется передать id Request для изменения статуса.

---
+++ src/Services/Request/Request.API/Endpoints/Request/ChangeCalledStatus.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.ApiEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Request.Application.Features.Requests.Commands.ChangeCalledStatus;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Request.API.Endpoints.Request;
+
+public class ChangeCalledStatus:EndpointBaseAsync
+    .WithRequest<ChangeCalledStatusCommand>
+    .WithResult<ActionResult>
+{
+    private readonly IMediator _mediator;
+
+
+    public ChangeCalledStatus(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("/request/calledStatus")]
+    [SwaggerOperation(
+        Summary = "Изменения статуса заявки",
+        Description = "Необходимо передать id заявки",
+        Tags = new[] { "Request" })
+    ]
+    public override async Task<ActionResult> HandleAsync(ChangeCalledStatusCommand request, CancellationToken cancellationToken = new CancellationToken())
+    {
+        var response = await _mediator.Send(request);
+        return Ok(response);
+    }
+}
---
+++ src/Services/Request/Request.Application/Features/Requests/Commands/ChangeCalledStatus/ChangeCalledStatusCommand.cs
@@ -0,0 +1,9 @@
+﻿using Ardalis.Result;
+using MediatR;
+
+namespace Request.Application.Features.Requests.Commands.ChangeCalledStatus;
+
+public class ChangeCalledStatusCommand : IRequest<Result>
+{
+    public int Id { get; set; }
+}
