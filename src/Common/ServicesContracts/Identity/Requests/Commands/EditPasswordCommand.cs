9be392d2ace80fa9f8d7774d6a4abd78f72c4ee9
Tota Jekeshova <t.jekeshova@gmail.com>
2022-10-18 19:03:05 +0600

    #13 Добавляет редактирование пароля пользователя (feature) - Реализовывает возможность изменения пароля зарегистированного пользователя

---
+++ src/Common/ServicesContracts/Identity/Requests/Commands/EditPasswordCommand.cs
@@ -0,0 +1,13 @@
+using Ardalis.Result;
+using MediatR;
+
+namespace ServicesContracts.Identity.Requests.Commands;
+
+public class EditPasswordCommand : IRequest<Result>
+{
+    public string Email { get; set; }
+    public string OldPassword { get; set; }
+    public string NewPassword { get; set; }
+    public string ConfirmPassword { get; set; }
+
+}
--- src/Gateway/SessionGatewayService/SessionGatewayService.Application/Contracts/IUserService.cs
+++ src/Gateway/SessionGatewayService/SessionGatewayService.Application/Contracts/IUserService.cs
@@ -9,4 +9,5 @@ public interface IUserService
     Task<DefaultResponceObject<UserVm>> GetUserAsync(string email, CancellationToken cancellationToken);
     Task<DefaultResponceObject<RegisterVm>> RegisterAsync(RegisterCommand command, CancellationToken cancellationToken);
     Task<DefaultResponceObject<string>> EditAsync(EditCommand command, CancellationToken cancellationToken);
+    Task<DefaultResponceObject<string>> EditPasswordAsync(EditPasswordCommand command, CancellationToken cancellationToken);
 }
