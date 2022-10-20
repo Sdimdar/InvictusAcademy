0cf547502d6e9c851555ed38b997adf2febeaea3
miha <miha890r@gmail.com>
2022-10-20 12:53:22 +0600

    #19 Создание Common MongoBaseRepository (feature)

---
+++ src/Common/CommonRepository/Abstractions/IMongoBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using CommonRepository.Models;
+
+namespace CommonRepository.Abstractions;
+
+public interface IMongoBaseRepository<T> where T : MongoBaseRepositoryEntity
+{
+    public Task<List<T>> GetAsync();
+
+    public Task<T?> GetAsync(int id);
+
+    public Task CreateAsync(T newBook);
+
+    public Task UpdateAsync(int id, T updatedBook);
+
+    public Task RemoveAsync(int id);
+}
---
+++ src/Common/CommonRepository/Models/MongoBaseRepositoryEntity.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace CommonRepository.Models;
+
+public class MongoBaseRepositoryEntity
+{
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public int Id { get; set; }
+}
