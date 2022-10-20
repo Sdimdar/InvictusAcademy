de99a04603bfb9bb7e142966eff2004384fbc9ea
miha <miha890r@gmail.com>
2022-10-20 18:18:12 +0600

    #20 Создание страницы со списком курсов пользователя (feature, refactor) - Создание страницы со списком курсов пользователя - Небольшой рефакторинг отображения страницы после выполнения запроса "залогинен ли пользователь"

---
+++ src/Frontend/InvuctusAcademy/src/components/CourseCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <q-card class="my-card" flat bordered>
+            <q-card-section horizontal class="flex justify-between">
+                <q-card-section class="q-pt-xs">
+                    <div class="text-h5 q-mt-sm q-mb-xs">{{ title }}</div>
+                    <div class="text-caption text-grey">
+                        {{ courseDescription }}
+                    </div>
+                </q-card-section>
+
+                <q-card-section class="col-5 flex flex-center">
+                    <q-img class="rounded-borders course-img" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+                </q-card-section>
+            </q-card-section>
+
+            <q-separator />
+
+            <q-card-actions class="flex justify-end">
+                <q-btn flat color="primary">
+                    Перейти к курсу
+                </q-btn>
+            </q-card-actions>
+        </q-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "TestCourse",
+            courseDescription: "Some Description about course"
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .course-img{
+        height: 100px;
+        width: 500px;
+    }
+</style>
---
+++ src/Frontend/InvuctusAcademy/src/pages/User/UserCoursesPage.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h4 style="text-align: center;">Ваши курсы</h4>
+
+    <q-splitter v-model="splitterModel" style="height: 750px">
+
+      <template v-slot:before>
+        <q-tabs v-model="tab" vertical class="text-teal">
+          <q-tab name="current" icon="mail" label="Текущие" />
+          <q-tab name="completed" icon="alarm" label="Завершённые" />
+          <q-tab name="wish" icon="movie" label="Желаемые" />
+        </q-tabs>
+      </template>
+
+      <template v-slot:after>
+        <q-tab-panels v-model="tab" animated swipeable vertical transition-prev="jump-up" transition-next="jump-up">
+          <q-tab-panel name="current">
+            <div class="text-h4 q-mb-md">Текущие</div>
+            <course-card />
+          </q-tab-panel>
+
+          <q-tab-panel name="completed">
+            <div class="text-h4 q-mb-md">Завершённые</div>
+            <course-card />
+          </q-tab-panel>
+
+          <q-tab-panel name="wish">
+            <div class="text-h4 q-mb-md">Желаемые</div>
+            <course-card />
+          </q-tab-panel>
+        </q-tab-panels>
+      </template>
+
+    </q-splitter>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import CourseCard from 'components/CourseCard.vue'
+
+export default {
+  props: {
+    logined : {
+      Type : Boolean
+    }
+  },
+  setup() {
+    return {
+      tab: ref("current"),
+      splitterModel: ref(20)
+    };
+  },
+  mounted(){
+    if(!this.logined)
+    {
+      this.$router.push({ name: 'homepage' })
+    }
+  },
+  components: { CourseCard }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
