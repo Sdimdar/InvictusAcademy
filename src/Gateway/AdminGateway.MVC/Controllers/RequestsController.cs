a944551763fc56a1a2c6c1d0b9c45e71a33294ce
Svoyevolin Dmitriy <sdimdar@gmail.com>
2022-10-23 12:34:21 +0600

    #23 Изменения ответов контроллера(refactor) - Изменил ответы контроллеров для Request service, ответы отдаются по единному типу DefaultResponseObject

--- src/Gateway/AdminGateway.MVC/Controllers/RequestsController.cs
+++ src/Gateway/AdminGateway.MVC/Controllers/RequestsController.cs
@@ -2,14 +2,12 @@
 using AdminGateway.MVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using ServicesContracts.Request.Requests.Commands;
-using ServicesContracts.Request.Responses;
 
 namespace AdminGateway.MVC.Controllers;
 
 public class RequestsController : Controller
 {
     private readonly IRequestService _requestService;
-
     public RequestsController(IRequestService requestService)
     {
         _requestService = requestService;
@@ -20,29 +18,23 @@ public class RequestsController : Controller
     {
         try
         {
-            var responce = await _requestService.GetAllRequestsAsync(pageNumber, pageSize);
-            if (responce is null)
-            {
-                ErrorVM error = new ErrorVM("Requests couldn't find");
-                return View("../Errors/ErrorPage", error);
-            }
-            var responceCount = await _requestService.GetRequestsCountAsync();
-            if (responceCount is null)
-            {
-                ErrorVM error = new ErrorVM("Requests count couldn't find");
-                return View("../Errors/ErrorPage", error);
-            }
-            GetAllRequestVm requests = new GetAllRequestVm
-            {
-                PageNumber = pageNumber,
-                PageSize = pageSize,
-                Requests = responce.Value.Requests,
-                TotalPages = (int)Math.Ceiling(responceCount.Value / (double)pageSize),
-                RequestsCount = responceCount.Value
-            };
-            TempData["pageNumber"] = pageNumber;
-            TempData["pageSize"] = pageSize;
-            return View(requests);
+            var response = await _requestService.GetAllRequestsAsync(pageNumber, pageSize);
+            return Ok(response);
+        }
+        catch (Exception e)
+        {
+            ErrorVM error = new ErrorVM(e.Message);
+            return View("../Errors/ErrorPage", error);
+        }
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetRequestsCount()
+    {
+        try
+        {
+            var response = await _requestService.GetRequestsCountAsync();
+            return Ok(response);
         }
         catch (Exception e)
         {
@@ -56,19 +48,13 @@ public class RequestsController : Controller
     {
         try
         {
-            if (command is null)
-            {
-                ErrorVM error = new ErrorVM("ViewModel is null");
-                return View("../Errors/ErrorPage", error);
-            }
-            if (command.Id == 0)
+            if (command.Id <= 0)
             {
                 ErrorVM error = new ErrorVM("Id was not assigned");
                 return View("../Errors/ErrorPage", error);
             }
-            await _requestService.ChangeCalledStatusAsync(command);
-            await GetAll(Convert.ToInt32(TempData["pageNumber"]), Convert.ToInt32(TempData["pageSize"]));
-            return PartialView("GetAll");
+            var response = await _requestService.ChangeCalledStatusAsync(command);
+            return Ok(response);
         }
         catch (Exception e)
         {
@@ -82,24 +68,14 @@ public class RequestsController : Controller
     {
         try
         {
-            if (command is null)
-            {
-                ErrorVM error = new ErrorVM("ViewModel is null");
-                return View("../Errors/ErrorPage", error);
-            }
-            if (command.Id == 0)
+           
+            if (command.Id <= 0)
             {
                 ErrorVM error = new ErrorVM("Id was not assigned");
                 return View("../Errors/ErrorPage", error);
             }
-            if (string.IsNullOrEmpty(command.ManagerComment))
-            {
-                ErrorVM error = new ErrorVM("ManagerComment must not be empty");
-                return View("../Errors/ErrorPage", error);
-            }
-            await _requestService.ManagerCommentAsync(command);
-            await GetAll(Convert.ToInt32(TempData["pageNumber"]), Convert.ToInt32(TempData["pageSize"]));
-            return PartialView("GetAll");
+            var response = await _requestService.ManagerCommentAsync(command);
+            return Ok(response);
         }
         catch (Exception e)
         {
