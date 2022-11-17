cde910afde35212455e281d318ceabf629a008c0
Svoyevolin Dmitriy <sdimdar@gmail.com>
2022-11-18 00:08:28 +0600

    #44 заготовка для оплат на админе

--- src/Frontend/InvictusAdmin/src/pages/Admin/PaymentRequestPage.vue
+++ src/Frontend/InvictusAdmin/src/pages/Admin/PaymentRequestPage.vue
@@ -8,7 +8,44 @@
       :columns="columns"
       row-key="id"
       v-model:pagination="pagination"
+      :loading="loading"
+      binary-state-sort
+      @request="onRequest"
     >
+    <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="id" :props="props">
+            {{ props.row.id }}
+          </q-td>
+          <q-td key="userEmail" :props="props">
+            {{ props.row.userEmail }}
+          </q-td>
+          <q-td key="courseId" :props="props">
+            {{ props.row.courseId }}
+          </q-td>
+          <q-td key="courseName" :props="props">
+            {{ props.row.courseName }}
+          </q-td>
+          <q-td key="managerComment" :props="props" v-if="props.row.purchase">
+            <q-input v-model="props.row.managerComment" type="text" 
+            @keyup.enter='managerCommentHadler(props.row.id, props.row.managerComment, $event)'/>
+          </q-td>
+          <q-td key="purchase" :props="props" v-if="!props.row.purchase">
+            <q-btn
+            v-model="props.row.purchase"
+            @click="changeCalledHadler(props.row.id)"
+            color="secondary"
+            >Подтвердить оплату</q-btn>
+          </q-td>
+          <q-td key="purchase" :props="props" v-if="props.row.purchase">
+            <q-btn
+            v-model="props.row.purchase"
+            @click="changeCalledHadler(props.row.id)"
+            color="deep-orange"
+            >Отменить оплату</q-btn>
+          </q-td>
+        </q-tr>
+      </template>
     </q-table>
 </div>
 </template>
@@ -24,38 +61,82 @@ const columns = [
     {name:'purchase', align: 'center', label: 'Подтверждение оплаты', field: 'purshase', sortable: false},
     ]
 
-    let request = [{
-    id:1,
-    userEmail:'test@email',
-    courseId:2,
-    courseName:'First Aid',
-    ManagerComment:'Wahahhaha',
-    purchase: 'HMMM'
-},
-{
-    id:2,
-    userEmail:'testSECIND@email',
-    courseId:3,
-    courseName:'SECOND Aid',
-    ManagerComment:'Wahahhaha',
-    purchase: 'HMMM' 
-}];
-let rows = [];
 
 export default{
     name: 'PaymentPage',
-    data(){
-        return{
-            columns,
-            rows
+    setup(){
+    const tableRef = ref()
+    const rows = ref([])
+    const filter = ref('')
+    const loading = ref(false)
+    const pagination = ref({
+      sortBy: 'desc',
+      descending: false,
+      page: 1,
+      rowsPerPage: 10,
+      rowsNumber: 10
+    })
+
+async function onRequest (props) {
+      console.log(props)
+      let { page, rowsPerPage, sortBy, descending } = props.pagination
+      let response;
+      loading.value = true
+      // update rowsCount with appropriate value
+      try {
+        response = await fetchRequestsCount();
+        console.log("Response:")
+        console.log(response)
+        if (response.data.isSuccess) {
+          pagination.value.rowsNumber = response.data.value;
         }
-    },
-    methods: {
-        getData(){
-            this.rows.push(...request)
-            console.log(request)
-            console.log(this.rows)
+        else {
+          response.data.errors.forEach(element => { notify.showErrorNotify(element); });
+          return;
         }
+      } catch (error) {
+        console.log(error.message);
+      }
+
+      // fetch data from "server"
+      try {
+        
+          response = await fetchAllRequest(page, rowsPerPage)
+        if (response.data.isSuccess) {
+          rows.value.splice(0, rows.value.length, ...response.data.value.requests);
+        }
+        else {
+          response.data.errors.forEach(element => { notify.showErrorNotify(element); });
+          return;
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+      
+      // don't forget to update local pagination object
+      pagination.value.page = page
+      pagination.value.rowsPerPage = rowsPerPage
+      pagination.value.sortBy = sortBy
+      pagination.value.descending = descending
+      // ...and turn of loading indicator
+      loading.value = false
+    }
+
+    onMounted(() => {
+      // get initial data from server (1st page)
+      tableRef.value.requestServerInteraction()
+    })
+    return {
+      tableRef,
+      filter,
+      loading,
+      pagination,
+      columns,
+      rows,
+      onRequest      
+    }
+},methods: {
+        
     },
     
 }
