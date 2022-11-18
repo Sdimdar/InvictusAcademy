49968eed52e3515c32e9e7c5c614e9f76390f2d6
Tota Jekeshova <t.jekeshova@gmail.com>
2022-11-19 00:56:27 +0600

    #42: Добавлена форма добавления тестов (feature)

--- src/Frontend/InvictusAdmin/src/components/Module/CreateTestButton.vue
+++ src/Frontend/InvictusAdmin/src/components/Module/CreateTestButton.vue
@@ -12,23 +12,29 @@
           <q-input dense v-model="test.testShowCount"  type="number" label="Количество вопросов для пользователя" />
           <q-input dense v-model="test.testCompleteCount"  type="number" label="Количество вопросов для успешного прохождения" />
 
-          <div v-for="question in test.testQuestions">
-          <q-input dense v-model="testQuestion.question" label="Ввeдите вопрос" />
-          <q-select
-            v-model="testQuestion.questionType"
-            :options="types"
-            label="Выберите тип вопроса"
-          />
-        </div>
+          <div  v-for="(input, index) in test.testQuestions" :key="`questionInput-${index}`">
+            <q-input dense v-model="input.question" label="Введите вопрос" />
+            <q-select  v-model="input.questionType" :options="types" label="Выберите тип вопроса" />
 
+            <div v-for="(answerInput, answerIndex) in input.answers" :key="`answerInput-${answerIndex}`">
+            <q-input  v-model="answerInput.text" dense label="Вариант ответа" />
+            <q-checkbox  v-model="answerInput.isCorrect" size="xs" label="Отметить как правильный" />
+            </div>
 
-          <q-input dense v-model="answer.text" label="Вариант ответа" />
+            <q-btn @click="addField(answerInput, input.answers)" label="Добавить ответ" />
+            <q-btn @click="removeField(answerIndex, input.answers)" label="Удалить ответ" />
 
+          </div>
+
+          <q-card-actions align="right" class="text-primary">
+            <q-btn @click="addField(input, test.testQuestions)"  label="Добавить вопрос" />
+            <q-btn @click="removeField(index, test.testQuestions)" label="Удалить вопрос" />
+          </q-card-actions>
 
         </q-card-section>
-        <q-card-actions align="right" class="text-primary">
-          <q-btn flat type="reset" label="Отмена" />
+        <q-card-actions align="center" class="text-primary">
           <q-btn flat type="submit" label="Сохранить" />
+          <q-btn flat type="reset" label="Отмена" />
         </q-card-actions>
       </q-form>
     </q-card>
@@ -47,7 +53,6 @@ export default defineComponent({
         required: true,
       },
       id: {
-        type: Number,
         required: true,
       },
     },
@@ -59,16 +64,7 @@ export default defineComponent({
         test:{
           testShowCount:"",
           testCompleteCount:"",
-          testQuestions: [{
-            testQuestion:{
-              question: "",
-              questionType:"",
-              answers: [{
-                text:"",
-                isCorrect:""
-              }]
-          },
-          }]
+          testQuestions: [{question: "", questionType: "Single",  answers:[{text: "", isCorrect: false}]}]
         },
         types: ["Single", "Multiple"],
       };
@@ -82,11 +78,17 @@ export default defineComponent({
       console.log(this.article)
     },
     async onSubmit() {
-
+      console.log(this.test)
     },
     onReset() {
       this.createTestDialog = false;
       this.errorMessage = "";
+    },
+    addField(value, fieldType) {
+      fieldType.push({value: {} });
+    },
+    removeField(index, fieldType) {
+      fieldType.splice(index, 1);
     }
   },
 });
