993b8e1535ddb31d5ffb1404878e7c081afc8337
miha <miha890r@gmail.com>
2022-11-25 18:06:36 +0600

    #57 Создание Endpoint для покупке курса (feature) - Создание Endpoint для создания таблиц о покупке курса - Вызов данного Endpoint в AdminGateway

--- src/Gateway/AdminGateway.MVC/Services/PaymentService.cs
+++ src/Gateway/AdminGateway.MVC/Services/PaymentService.cs
@@ -1,7 +1,9 @@
 ﻿using AdminGateway.MVC.Services.Interfaces;
+using Ardalis.Result;
 using AutoMapper;
 using DataTransferLib.Models;
 using ExtendedHttpClient;
+using ServicesContracts.Courses.Requests.Courses.Commands;
 using ServicesContracts.Courses.Requests.Courses.Querries;
 using ServicesContracts.Courses.Responses;
 using ServicesContracts.Payments.Commands;
@@ -14,42 +16,71 @@ public class PaymentService : IPaymentService
 {
     public ExtendedHttpClient<IPaymentService> ExtendedHttpClient { get; set; }
     public ExtendedHttpClient<ICoursesService> CourseHttpClient { get; set; }
-    public PaymentService(ExtendedHttpClient<IPaymentService> httpClient, ExtendedHttpClient<ICoursesService> courseHttpClient)
+    private readonly IMapper _mapper;
+    
+    public PaymentService(ExtendedHttpClient<IPaymentService> httpClient, 
+                          ExtendedHttpClient<ICoursesService> courseHttpClient, 
+                          IMapper mapper)
     {
         ExtendedHttpClient = httpClient;
         CourseHttpClient = courseHttpClient;
+        _mapper = mapper;
     }
     
     public async Task<DefaultResponseObject<bool>> AddPaymentRequestAsync(AddPaymentCommand request, 
-                                                                     CancellationToken cancellationToken)
+                                                                          CancellationToken cancellationToken)
     {
         return await ExtendedHttpClient.PostAndReturnResponseAsync<AddPaymentCommand, DefaultResponseObject<bool>>
             (request, "/Payments/Add", cancellationToken);
     }
 
     public async Task<DefaultResponseObject<bool>> ConfirmPaymentRequestAsync(ConfirmPaymentCommand request, 
-                                                                         CancellationToken cancellationToken)
+                                                                              CancellationToken cancellationToken)
     {
-        return await ExtendedHttpClient.PostAndReturnResponseAsync<ConfirmPaymentCommand, DefaultResponseObject<bool>>
-            (request, "/Payments/Confirm", cancellationToken);
+        var paymentConfirmResult = await ExtendedHttpClient.PostAndReturnResponseAsync
+            <ConfirmPaymentCommand, DefaultResponseObject<bool>>(request, "/Payments/Confirm", cancellationToken);
+        if (!paymentConfirmResult.IsSuccess) return paymentConfirmResult;
+        
+        GetPaymentQuery query = new()
+        {
+            PaymentId = request.PaymentId
+        };
+        var paymentInfo = await GetByIdPaymentRequestAsync(query, cancellationToken);
+        if (!paymentInfo.IsSuccess)
+        {
+            if (paymentInfo.Errors is not null)
+            {
+                return _mapper.Map<DefaultResponseObject<bool>>(Result.Error(paymentInfo.Errors));
+            }
+            return _mapper.Map<DefaultResponseObject<bool>>(Result.Invalid(paymentInfo.ValidationErrors!.ToList()));
+        }
+        PurchaseCourseCommand purchaseCourseCommand = new()
+        {
+            CourseId = paymentInfo.Value!.CourseId,
+            //TODO: Раскомментировать после того как UserEmail будет заменён на UserId
+            //UserId = paymentInfo.Value!.UserId
+        };
+        return await CourseHttpClient.PostAndReturnResponseAsync
+            <PurchaseCourseCommand, DefaultResponseObject<bool>>(purchaseCourseCommand, "/Course/Purchase", cancellationToken);
+        
     }
 
     public async Task<DefaultResponseObject<bool>> RejectPaymentRequestAsync(RejectPaymentCommand request, 
-                                                                        CancellationToken cancellationToken)
+                                                                             CancellationToken cancellationToken)
     {
         return await ExtendedHttpClient.PostAndReturnResponseAsync<RejectPaymentCommand, DefaultResponseObject<bool>>
             (request, "/Payments/Reject", cancellationToken);
     }
 
     public async Task<DefaultResponseObject<PaymentVm>> GetByIdPaymentRequestAsync(GetPaymentQuery request, 
-                                                                              CancellationToken cancellationToken)
+                                                                                   CancellationToken cancellationToken)
     {
         return await ExtendedHttpClient.PostAndReturnResponseAsync<DefaultResponseObject<PaymentVm>>
             ($"/Payments/Get?PaymentId={request.PaymentId}", cancellationToken);
     }
 
     public async Task<DefaultResponseObject<List<PaymentsVm>>> GetWithParametersPaymentRequestAsync(GetPaymentsWithParametersQuery request, 
-                                                                                              CancellationToken cancellationToken)
+                                                                                                    CancellationToken cancellationToken)
     {
         var payments = await ExtendedHttpClient.GetAndReturnResponseAsync<DefaultResponseObject<List<PaymentsVm>>>(
             $"/Payments/GetWithParameters?UserEmail={request.UserEmail}&CourseId={request.CourseId}&Status={request.Status}",
@@ -62,7 +93,7 @@ public class PaymentService : IPaymentService
         }
         GetCoursesByIdListQuery internalRequest = new GetCoursesByIdListQuery { CoursesId = list };
         var coursesNames = await CourseHttpClient.PostAndReturnResponseAsync<GetCoursesByIdListQuery, DefaultResponseObject<List<CoursesByIdVm>>>(internalRequest,
-            "/Course/GetCoursesById");
+            "/Course/GetCoursesById", cancellationToken);
         if (!coursesNames.IsSuccess)
         {
             if (coursesNames.Errors.Any()) payments.Errors = coursesNames.Errors;
---
+++ src/Services/Courses/Courses.API/Endpoints/Course/PurchaseCourse.cs
@@ -0,0 +1,36 @@
+﻿using Ardalis.ApiEndpoints;
+using AutoMapper;
+using DataTransferLib.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ServicesContracts.Courses.Requests.Courses.Commands;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Courses.API.Endpoints.Course;
+
+public class PurchaseCourse : EndpointBaseAsync
+    .WithRequest<PurchaseCourseCommand>
+    .WithActionResult<DefaultResponseObject<bool>>
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public PurchaseCourse(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+    
+    [HttpPost("/Course/Purchase")]
+    [SwaggerOperation(
+        Summary = "Добавление информации в бд о том что курс успешно куплен",
+        Description = "Необходимо передать в теле запроса Id курса и Id пользователя",
+        Tags = new[] { "Course" })
+    ]
+    public override async Task<ActionResult<DefaultResponseObject<bool>>> HandleAsync(PurchaseCourseCommand request, 
+                                                                                      CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(request, cancellationToken);
+        return Ok(_mapper.Map<DefaultResponseObject<bool>>(result));
+    }
+}
