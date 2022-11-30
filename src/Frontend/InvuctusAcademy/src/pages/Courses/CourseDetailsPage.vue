22cd5f0567a57f804a8b6441c1e5e19b6bc35ee7
Tota Jekeshova <t.jekeshova@gmail.com>
2022-11-30 17:39:40 +0600

    #51: Исправлена страница с описанием курса (fix)

--- src/Frontend/InvuctusAcademy/src/pages/Courses/CourseDetailsPage.vue
+++ src/Frontend/InvuctusAcademy/src/pages/Courses/CourseDetailsPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page-container class="column" style="height: 1500px; padding-bottom: 0px;">
+  <q-page-container class="column" style="height: 1800px; padding-bottom: 0px;">
 
       <div class="col">
         <div class="row" style="margin-left: 20px;">
@@ -13,6 +13,41 @@
             <div style="font-size: 16px; font-weight: 300; color: #000000; margin-top: 15px;">
                {{ course.description }}
             </div>
+            <div class="row">
+              <div class="col-5">
+                <div class="column">
+                  <div class="col-2 small-text">
+                    Дата начала:
+                  </div>
+                  <div class="col big-text">
+                    <p> В любое время </p>
+                </div>
+              </div>
+              </div>
+              <div class="col-3">
+                <div class="column">
+                  <div class="col-2 small-text">
+                    Формат:
+                  </div>
+                  <div class="col big-text">
+                    <p> Онлайн </p>
+                </div>
+              </div>
+              </div>
+              <div class="col-4">
+                <div class="column">
+                  <div class="col-2 small-text">
+                    Количество модулей:
+                  </div>
+                  <div class="col big-text">
+                    <p> {{courseModules.length}}</p>
+                </div>
+              </div>
+              </div>
+            </div>
+            <div>
+              <q-btn no-caps color="accent" class="start-btn" label="Начать обучение" />
+            </div>
           </div>
 
           <div class="column" style="width: 35%;">
@@ -21,14 +56,58 @@
         </div>
       </div>
 
-      <div class="col"> Мой прогресс </div>
-      <div class="col">
+      <div class="col" style="margin-left: 20px;">
+        <div class="row">
+              <div class="col-3">
+                <div class="column">
+                  <div class="col" style="font-size: 22px; font-weight: 600;">
+                    Самая актуальная информация
+                  </div>
+                  <div class="col" style="font-size: 16px; font-weight: 300;">
+                    <p> С учётом всех изменений <br /> на рынке </p>
+                </div>
+              </div>
+              </div>
+              <div class="col-3">
+                <div class="column">
+                  <div class="col" style="font-size: 22px; font-weight: 600;">
+                    Гарантия трудоустройства
+                  </div>
+                  <div class="col" style="font-size: 16px; font-weight: 300;">
+                    <p> И персональный ментор, <br />  который всегда на связи </p>
+                </div>
+              </div>
+              </div>
+              <div class="col-3">
+                <div class="column">
+                  <div class="col" style="font-size: 22px; font-weight: 600;">
+                    Возможность <br />  учиться удалённо
+                  </div>
+                  <div class="col" style="font-size: 16px; font-weight: 300;">
+                    <p>Из любой точки мира, <br />  нужен всего-лишь вай-фай</p>
+                </div>
+              </div>
+              </div>
+              <div class="col-3">
+                <div class="column">
+                  <div class="col" style="font-size: 22px; font-weight: 600;">
+                    Востребованный сертификат
+                  </div>
+                  <div class="col" style="font-size: 16px; font-weight: 300;">
+                    <p > По окончанию курса <br /> вы получите сертификат </p>
+                </div>
+              </div>
+              </div>
+            </div>
+        </div>
+
+      <div class="col-2">
         Идеально подойдут вам
           <div class="row">
 
           </div>
       </div>
-      <div class="col"> Недавно просмотренные </div>
+      <div class="col-2"> Недавно просмотренные </div>
       <div class="col-2"> Читайте также </div>
   </q-page-container>
 
@@ -36,14 +115,14 @@
 
 <script>
 import { ref } from "vue";
-import { getCourseById } from "boot/axios";
+import { getCourseById, getShortModulesInfo } from "boot/axios";
 
 export default {
   data() {
     return {
       id: Number(this.$route.query.id),
       course:"",
-
+      courseModules: []
     };
   },
   mounted() {
@@ -54,6 +133,9 @@ export default {
       const response = await getCourseById(this.id);
       console.log(response.data)
       this.course = response.data.value
+      const modulesresponse = await getShortModulesInfo(this.id);
+      console.log(modulesresponse.data)
+      this.courseModules = modulesresponse.data.value
     },
   },
 };
@@ -61,9 +143,28 @@ export default {
 
 <style>
 .preview{
-  height: 300px;
+  height: 320px;
   width: 260px;
   padding: 10px;
 }
+.small-text{
+  font-size: 16px;
+  font-weight: 300;
+  color: #000000;
+  margin-top: 20px;
+}
+
+.big-text{
+  font-size: 24px;
+  font-weight: 700;
+  color: #000000;
+  margin-top: 10px;
+}
+.start-btn{
+  padding: 15px;
+  box-shadow: 0px 4px 22px rgba(177, 20, 20, 0.12);
+  border-radius: 10px;
+}
+
 </style>
 
