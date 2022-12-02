abe575f7cfabcd7f843e9e7bd0ed591d5cd76182
Madiar <madiar.bakbergenov10@hmail.com>
2022-12-02 19:21:07 +0600

    #73 Добавил DbMap

---
+++ src/Services/Jitsi/Jitsi.API/Models/DbMap/StreamingRoomDbMap.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Jitsi.API.Models.DbMap;
+
+public class StreamingRoomDbMap : IEntityTypeConfiguration<StreamingRoomDbMap>
+{
+    public void Configure(EntityTypeBuilder<StreamingRoomDbMap> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
