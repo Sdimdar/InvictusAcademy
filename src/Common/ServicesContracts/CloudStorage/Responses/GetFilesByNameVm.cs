1d40505c106e8bc7dcf4db7b2a86684e9b2059eb
Abzal Mautaliev <abzalmautaliev@outlook.com>
2022-12-18 04:01:51 +0600

    #72 удалил лишние файлы

--- src/Common/ServicesContracts/CloudStorage/Responses/GetFilesByNameVm.cs
+++
@@ -1,8 +0,0 @@
-using CloudStorage.Domain.Entities;
-
-namespace ServicesContracts.CloudStorage.Responses;
-
-public class GetFilesByNameVm
-{
-    public List<CloudStorageDbModel> Files { get; set; }
-}
