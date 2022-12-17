128b0315bfdc571d6480497473850489e23dfd47
Ryaguzov  Michael <miha890r@gmail.com>
2022-12-17 19:57:06 +0600

    Перевод сессий на Redis (feature) - Также логи замаплены на локальную папку на ПК - Папка с логами добавлена в .gitignore

--- src/Gateway/UserGateway/UserGateway.Infrastructure/DependencyInjection.cs
+++ src/Gateway/UserGateway/UserGateway.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,4 @@
-﻿using Community.Microsoft.Extensions.Caching.PostgreSql;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using UserGateway.Application.Contracts;
@@ -11,13 +10,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDistributedPostgreSqlCache(options =>
+        services.AddStackExchangeRedisCache(options =>
         {
-            options.ConnectionString = configuration.GetConnectionString("SessionConnectionString");
-            options.SchemaName = configuration["SchemaName"];
-            options.TableName = configuration["TableName"];
-            options.CreateInfrastructure = true;
+            options.Configuration = configuration.GetConnectionString("RedisCacheConnectionString");
         });
+
         services.AddTransient<IPaymentService, PaymentService>();
 
         return services;
