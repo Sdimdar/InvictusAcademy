4790166b9dc0e298f22833c73cc5d12c81adad04
Tota Jekeshova <t.jekeshova@gmail.com>
2022-12-19 22:38:04 +0600

    #75: Доработана страница с детализацией курса (fix)

---
+++ src/UnitTests/Identity.API.Tests/EditTests.cs
@@ -0,0 +1,258 @@
+﻿using ServicesContracts.Identity.Requests.Commands;
+
+namespace User.API.Tests;
+
+public class EditTests : IClassFixture<CustomApplicationFactory<Program>>
+{
+    private readonly ExtendedHttpClientForTests _httpClient;
+
+    public EditTests(CustomApplicationFactory<Program> factory)
+    {
+        _httpClient = new ExtendedHttpClientForTests(factory.CreateClient());
+    }
+
+    [Fact]
+    public async Task Edit_SendRequestWithCorrectData()
+    {
+        // Arrange
+
+        EditCommand command = new()
+        {
+            Email = "test@mail.ru",
+            FirstName = "Famine",
+            MiddleName = "Famine",
+            LastName = "Famine",
+            InstagramLink = null,
+            Citizenship = "Казахстан",
+            PhoneNumber = "82739348372"
+        };
+
+        // Act
+        var data = await _httpClient.PostAndReturnResponseAsync<EditCommand, string>(command, "/User/Edit");
+
+        // Assert
+        data.Should().NotBeNull();
+        data.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Edit_SendRequestWithInvalidEmail()
+    {
+        // Arrange
+
+        EditCommand command = new()
+        {
+            Email = "testi@mail.ru",
+            FirstName = "Famine",
+            MiddleName = "Famine",
+            LastName = "Famine",
+            InstagramLink = null,
+            Citizenship = "Казахстан",
+            PhoneNumber = "82739348372"
+        };
+
+        // Act
+        var data = await _httpClient.PostAndReturnResponseAsync<EditCommand, string>(command, "/User/Edit");
+
+        // Assert
+        data.Should().NotBeNull();
+        data.IsSuccess.Should().BeFalse();
+        data.Errors.Should().NotBeEmpty();
+    }
+
+    public static IEnumerable<object[]> InvalidPhoneNumbers()
+    {
+        yield return new object[] { "12931" };
+        yield return new object[] { "asd" };
+        yield return new object[] { "" };
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidPhoneNumbers))]
+    public async Task Edit_SendRequestWithInvalidPhoneNumber(string invalidNumber)
+    {
+        // Arrange
+
+        EditCommand command = new()
+        {
+            Email = "test@mail.ru",
+            FirstName = "Famine",
+            MiddleName = "Famine",
+            LastName = "Famine",
+            InstagramLink = null,
+            Citizenship = "Казахстан",
+            PhoneNumber = invalidNumber
+        };
+
+        // Act
+        var data = await _httpClient.PostAndReturnResponseAsync<EditCommand, string>(command, "/User/Edit");
+
+        // Assert
+        data.Should().NotBeNull();
+        data.IsSuccess.Should().BeFalse();
+        data.ValidationErrors.Should().NotBeNull();
+        data.ValidationErrors.Should().Contain(e => e.Identifier == "PhoneNumber");
+    }
+
+    public static IEnumerable<object[]> InvalidFirstNames()
+    {
+        yield return new object[] { "" };
+        yield return new object[] { new string('s', 300) };
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidFirstNames))]
+    public async Task Edit_SendRequestWithInvalidFirstName(string invalidFirstName)
+    {
+        // Arrange
+
+        EditCommand command = new()
+        {
+            Email = "test@mail.ru",
+            FirstName = invalidFirstName,
+            MiddleName = "Famine",
+            LastName = "Famine",
+            InstagramLink = null,
+            Citizenship = "Казахстан",
+            PhoneNumber = "82739348372"
+        };
+
+        // Act
+        var data = await _httpClient.PostAndReturnResponseAsync<EditCommand, string>(command, "/User/Edit");
+
+        // Assert
+        data.Should().NotBeNull();
+        data.IsSuccess.Should().BeFalse();
+        data.ValidationErrors.Should().NotBeNull();
+        data.ValidationErrors.Should().Contain(e => e.Identifier == "FirstName");
+    }
+
+    public static IEnumerable<object[]> InvalidMiddleNames()
+    {
+        yield return new object[] { new string('s', 300) };
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidMiddleNames))]
+    public async Task Edit_SendRequestWithInvalidMiddleName(string invalidMiddleName)
+    {
+        // Arrange
+
+        EditCommand command = new()
+        {
+            Email = "test@mail.ru",
+            FirstName = "Famine",
+            MiddleName = invalidMiddleName,
+            LastName = "Famine",
+            InstagramLink = null,
+            Citizenship = "Казахстан",
+            PhoneNumber = "82739348372"
+        };
+
+        // Act
+        var data = await _httpClient.PostAndReturnResponseAsync<EditCommand, string>(command, "/User/Edit");
+
+        // Assert
+        data.Should().NotBeNull();
+        data.IsSuccess.Should().BeFalse();
+        data.ValidationErrors.Should().NotBeNull();
+        data.ValidationErrors.Should().Contain(e => e.Identifier == "MiddleName");
+    }
+
+    public static IEnumerable<object[]> InvalidLastNames()
+    {
+        yield return new object[] { "" };
+        yield return new object[] { new string('s', 300) };
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidLastNames))]
+    public async Task Edit_SendRequestWithInvalidLastName(string invalidLastName)
+    {
+        // Arrange
+
+        EditCommand command = new()
+        {
+            Email = "test@mail.ru",
+            FirstName = "Famine",
+            MiddleName = "Famine",
+            LastName = invalidLastName,
+            InstagramLink = null,
+            Citizenship = "Казахстан",
+            PhoneNumber = "82739348372"
+        };
+
+        // Act
+        var data = await _httpClient.PostAndReturnResponseAsync<EditCommand, string>(command, "/User/Edit");
+
+        // Assert
+        data.Should().NotBeNull();
+        data.IsSuccess.Should().BeFalse();
+        data.ValidationErrors.Should().NotBeNull();
+        data.ValidationErrors.Should().Contain(e => e.Identifier == "LastName");
+    }
+
+    public static IEnumerable<object[]> InvalidInstagramLinks()
+    {
+        yield return new object[] { new string('s', 300) };
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidInstagramLinks))]
+    public async Task Edit_SendRequestWithInvalidInstagramLink(string invalidInstagramLink)
+    {
+        // Arrange
+
+        EditCommand command = new()
+        {
+            Email = "test@mail.ru",
+            FirstName = "Famine",
+            MiddleName = "Famine",
+            LastName = "Famine",
+            InstagramLink = invalidInstagramLink,
+            Citizenship = "Казахстан",
+            PhoneNumber = "82739348372"
+        };
+
+        // Act
+        var data = await _httpClient.PostAndReturnResponseAsync<EditCommand, string>(command, "/User/Edit");
+
+        // Assert
+        data.Should().NotBeNull();
+        data.IsSuccess.Should().BeFalse();
+        data.ValidationErrors.Should().NotBeNull();
+        data.ValidationErrors.Should().Contain(e => e.Identifier == "InstagramLink");
+    }
+
+    public static IEnumerable<object[]> InvalidCitizenships()
+    {
+        yield return new object[] { new string('s', 300) };
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidCitizenships))]
+    public async Task Edit_SendRequestWithInvalidCitizenship(string invalidCitizenship)
+    {
+        // Arrange
+
+        EditCommand command = new()
+        {
+            Email = "test@mail.ru",
+            FirstName = "Famine",
+            MiddleName = "Famine",
+            LastName = "Famine",
+            InstagramLink = "",
+            Citizenship = invalidCitizenship,
+            PhoneNumber = "82739348372"
+        };
+
+        // Act
+        var data = await _httpClient.PostAndReturnResponseAsync<EditCommand, string>(command, "/User/Edit");
+
+        // Assert
+        data.Should().NotBeNull();
+        data.IsSuccess.Should().BeFalse();
+        data.ValidationErrors.Should().NotBeNull();
+        data.ValidationErrors.Should().Contain(e => e.Identifier == "Citizenship");
+    }
+}
